2657eda8fc17aa5d9e01c47f538cbf51b7956e4e
Jasbir <mr.jasbirsingh96@gmail.com>
2024-03-13 09:08:20 +0530

    added hw description

--- code/Lecture_3/2_dbDemo.js
+++ code/Lecture_3/2_dbDemo.js
@@ -17,10 +17,9 @@ const dbUrl =
     `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.drcvhxp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.connect(dbUrl).then(function (conn) {
-    console.log("connection : ", conn)
+    console.log("connected to db")
 }).catch(err => console.log(err))
 // connect with Database
-
 
 /*************userSchema****************/
 let userSchemaObject = {
@@ -44,22 +43,50 @@ let userSchemaObject = {
         validate: function () {
             return this.password == this.confirmPassword;
         }
+    },
+    createAt: {
+        type: Date,
+        default: Date.now()
+    },
+    role: {
+        type: String,
+        default: "user"
     }
 }
+
+
 const userSchema = new mongoose.Schema(userSchemaObject);
+const roles = ["admin", "buyer", "seller"];
+
+userSchema.pre("save", function () {
+    this.confirmPassword = undefined;
+})
+userSchema.pre("save", function (next) {
+    let isPresent = roles.find((cRole) => { return cRole == this.role })
+    if (isPresent == undefined) {
+        const error = new Error("role is invalid");
+        next(error);
+    }
+})
+
+
+
+
 // USERMODEL
 const UserModel = mongoose.model("MarchUserModel", userSchema);
 /***************handlers**********************/
-const getUser = (req, res) => {
+const getUser = async (req, res) => {
     try {
         // template -> get the data from req.params
         const id = req.params.id;
-        // finding the user
-        const user = userDataStore.find((userObject) => {
-            return userObject.id == id;
-        })
+        const user = await UserModel.findById(id);
         // if user is present -> send the resp
         if (user) {
+            user.password = undefined;
+            user.__v = undefined;
+            if (user.confirmPassword) {
+                user.confirmPassword = undefined
+            }
             res.status(200).json({
                 message: user
             })
@@ -67,6 +94,29 @@ const getUser = (req, res) => {
         } else {
             res.status(404).json({
                 message: "did not get the user"
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            status: "Internal server error",
+            message: err.message
+        })
+    }
+
+}
+const getAllUser = async (req, res) => {
+    try {
+
+        const user = await UserModel.find();
+        // if user is present -> send the resp
+        if (user.length != 0) {
+            res.status(200).json({
+                message: user
+            })
+            // if it's not there then send user not found 
+        } else {
+            res.status(404).json({
+                message: "did not get any user"
             })
         }
     } catch (err) {
@@ -95,28 +145,33 @@ const createUser = async (req, res) => {
     }
 
 }
+
 const updateUser = (req, res) => {
+    // await UserModel.findByIdAndUpdate(id,{},{new:true})
     console.log("Received patch request");
     console.log("body", req.body);
     res.json({
         message: "Recieved the patch request"
     })
 }
-const deleteUser = (req, res) => {
+const deleteUser = async (req, res) => {
     try {
         let { id } = req.params;
-
-        if (idx == -1) {
+        const user = await UserModel.findByIdAndDelete(id);
+        if (user === null) {
             res.status(404).json({
-                message: "did not get the user"
+                status: "sucess",
+                message: "user does not exist",
+
             })
         } else {
-
             res.status(200).json({
                 status: "sucess",
-                message: "user is deleted"
+                message: "user is deleted",
+                user: user
             })
         }
+
 
     } catch (err) {
         res.status(500).json({
@@ -149,6 +204,7 @@ app.use(express.json());
 app.post("/api/user",
     sanityMiddleWare,
     createUser);// profile page -> user
+app.get("/api/user", getAllUser);
 // 2. get the user
 app.get("/api/user/:id", getUser);
 // 3. update the user
@@ -174,3 +230,20 @@ app.listen(3000, function () {
 
 
 
+
+/***
+ * Ecommerce
+ *      * name
+ *      * brand
+ *      * description
+ *      * price
+ *      * discount
+ *      * categories 
+ * **/ 
+
+/***
+ * 1. Proper -> arch(MVC)
+ * 2. REST API -> 
+ * 3. DRY principle
+ * 
+ * **/ 
---
+++ code/Lecture_3/hw.md
@@ -0,0 +1,5 @@
+## Homework 
+
+## Problem Statement
+* Implement update user in our API
+* Add Product schema and CRUD methods for them as well 
